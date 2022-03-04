765189d669b19cb5055477c5bef8ebbb14284790
Kwadwo A. Yeboah-Asare Jnr <35611062+Prekaya@users.noreply.github.com>
2022-03-04 01:20:06 -0500

    Add files via upload

--- src/wall_follower_2.py
+++ src/wall_follower_2.py
@@ -8,12 +8,13 @@ from operator import truediv
 from turtle import end_fill
 import numpy as np
 # from scipy import linalg
-import math
+
 import rospy
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
+
 from visualization_tools import *
 
 class WallFollower:
@@ -27,182 +28,271 @@ class WallFollower:
     DESIRED_DISTANCE = rospy.get_param("wall_follower/desired_distance")
     WALL_TOPIC = "/wall"
 
+    laser_angle = 5*pi/8 # 5*pi/8 # the range we use to fit a line
+    laser_offset = pi/6
+
     vel_max = 4
     steering_max = 0.34
-    max_turn_rad = 0.9
     wheel_base = 0.325
     L1 = 1.6 # 1.6 seperation distance
     VLratio = VELOCITY/L1 # 1.6 & 3
+
     err_y_last = 0
     err_theta_last = 0
     last_ack_angle = 0
-    Pgain = 6
-    Dgain = 2
-    max_thresh = 2
-    min_thresh = 0.1
-    front = (0.5,0.1)
-    right = (0.2,0.75)
-    left = (0.35,0.8)
-    sweaty = 1.5
-    safety_rad = 2
-
+    Pgain = 1
+    Dgain = 5
+    thresh = 2
     def __init__(self):
         # TODO:
         # Initialize your publishers and subscribers here
-        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
-        self.line_pub = rospy.Publisher(self.WALL_TOPIC, Marker, queue_size=10)
-        rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback, (self.pub, self.line_pub))
+        pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
+        self.debug_pub = rospy.Publisher("debug", String, queue_size=10)
+        self.error_pub = rospy.Publisher("error", Float32, queue_size=10)
+        line_pub = rospy.Publisher(self.WALL_TOPIC, Marker, queue_size=1)
+        rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback, (pub, line_pub))
 
     # Write your callback functions here.
     def callback(self, msg, pub_list):
         turn_msg = AckermannDriveStamped()
+
+        
         pub = pub_list[0]
         line_pub = pub_list[1]
+        
+        wall = self.wall_in_front(msg)
+       
+        if wall[0]==True:
+            if self.SIDE == -1: # right wall
+                ack_angle = 0.3 # turn left
+            else:
+                ack_angle = -0.3
+            print("wall dectected")
+        else:   
+            k, b = self.find_line_aggr(msg, line_pub)
+            # ack_angle = self.LQRcontroller(k, b)
+            ack_angle = self.PDController(k, b)
 
-        angle_min = msg.angle_min
-        angle_max = msg.angle_max
-        angle_increment = msg.angle_increment
-        ranges = msg.ranges
-        angles = np.arange(angle_min, angle_max, angle_increment)
+
+        # steer angle: positive left, negative right
         turn_msg.header.stamp = rospy.Time.now()
-
-        stop = self.safety(ranges, angles)
-        if stop:
+        turn_msg.drive.steering_angle = ack_angle
+        #turn_msg.drive.speed = self.VELOCITY
+        turn_msg.drive.acceleration = 0.1
+        # for the safety controller
+        if wall[1]==True:
             turn_msg.drive.speed = 0 # safety
+            turn_msg.drive.steering_angle = 0
         else:
             turn_msg.drive.speed = self.VELOCITY
-            k, b = self.find_line_pub(ranges, angles)
-            ack_angle = self.PPController(k, b)
-            # steer angle: positive left, negative right
-            turn_msg.drive.steering_angle = ack_angle
-            
         pub.publish(turn_msg)       
 
-    def safety(self, ranges, angles):
-        """
-        r_ranges, r_angles = self.select_data(ranges,angles,self.right,0)
-        l_ranges, l_angles = self.select_data(ranges,angles,self.left,0)
-        rk, rb = self.find_line(r_ranges, r_angles)
-        lk, lb = self.find_line(l_ranges, l_angles)
-        ld = abs(lb)/math.sqrt(1+(lk**2))
-        rd = abs(rb)/math.sqrt(1+(rk**2))
-        L = self.wheel_base
-        n = self.steering_max
-        rd1 = (L/math.tan(n))*(1+(self.SIDE*rk)/math.sqrt(rk**2+1)) + L*math.tan(0.5*n)
-        ld1 = (L/math.tan(n))*(1+(-self.SIDE*lk)/math.sqrt(lk**2+1)) + L*math.tan(0.5*n)
-        if (rd1 - rd >= self.sweaty):
-            print("stopped")
-            return True
-        else:
-            return False
-        """
-        rho = []
-        phi = []
-        s_ranges, s_angles = self.select_data(ranges,angles,self.front,1)
-        for i in range(len(s_ranges)):
-                if (s_ranges[i] < self.safety_rad):
-                    rho.append(s_ranges[i])
-                    phi.append(s_angles[i])
-        L = self.wheel_base
-        check = len(rho)
-        if check:
-            x = rho*np.cos(phi)
-            y = rho*np.sin(phi)
-            coords = zip(x,y)
-            for x,y in coords:
-                out_circle = ((x+0.9)**2) + y**2 < ((0.9+L/2)**2)
-                inn_circle = ((x+0.9)**2) + y**2 > ((0.9-L/2)**2)
-                infront = y > 0
-                if out_circle and inn_circle and infront:
-                    return True
-            return False
-        else:
-            return False
-
-    def PPController(self, k, b):
+    def PDController(self, k, b):
+        # if self.SIDE == 1: # left with P controller
+            
         theta = np.arctan(k)
         err_y_now = np.abs(b*np.cos(theta)) - self.DESIRED_DISTANCE
         err_theta_now = theta
         err_dy = err_y_now - self.err_y_last
         a_cmd = 2*self.VLratio*(self.Dgain*err_dy + self.Pgain*self.VLratio*err_y_now) # assume theta_d is small
-        ackman_ang = a_cmd#self.L1*a_cmd/self.VELOCITY**2
+        ackman_ang = self.L1*a_cmd/self.VELOCITY**2
+
         self.err_y_last = err_y_now
         self.err_theta_last = err_theta_now
-        output = self.SIDE*ackman_ang
-        return output
 
-    def select_data(self, ranges, angles, (left,right), sel_type):
-        (offset,split) = (left,right) 
-        length = len(ranges)
-        high_ind = length - 1
+        if self.SIDE == -1: # right
+            return -ackman_ang
 
-        if sel_type == 0:
-        	start_ind = int(min(length*(left), high_ind))
-        	end_ind = int(min(length*(right),high_ind))
-        elif sel_type == 1:
-            start_ind = int(min(length*(offset-split), high_ind))
-            end_ind = int(min(length*(offset+split),high_ind))
+        return ackman_ang
 
-        cut_ranges = ranges[start_ind:end_ind]
-        cut_angles = angles[start_ind:end_ind]
-        return cut_ranges, cut_angles
+    # big problem: the system seems not controllable
+    # def LQRcontroller(self, k, b):
+    #     # x is current state for y and theta
 
-    """
-    def wall_in_front(self, ranges, angles):
-        ranges_front, angle_front = self.select_data(ranges,angles,self.front)
-        dist_sum  = np.cos(angle_front)*ranges_front
-        stop = sum(dist_sum)/(2*split+1) < self.wheel_base*1
-        turn = sum(dist_sum)/(2*split+1) < self.DESIRED_DISTANCE+self.wheel_base*1.5
-        return turn, stop
-    """
+    #     theta = np.arctan(k)
+    #     y_err = np.abs(b*np.cos(theta)) - self.DESIRED_DISTANCE
 
-    def find_line(self, ranges, angles):
-        # use min distance except when min_ind on wrong side
-        if self.SIDE == -1: # right wall
-        	line_ranges, line_angles = self.select_data(ranges,angles,self.right,0)
+    #     A = np.array([[0, np.cos(theta)*self.VELOCITY], [0,0]])
+    #     B = np.array([[0, self.VELOCITY/(np.cos(self.last_ack_angle)**2)]]).T
+    #     Q = np.array([[2,0], [0,50]])
+    #     R = np.array([[1000]])
+    #     # print(scipy.)
+    #     S = linalg.solve_continuous_are(A,B,Q,R)
+    #     K = np.dot(np.linalg.inv(R), np.dot(B.T, S))
+
+    #     ack_angle = -np.dot(K, np.array([[y_err, theta]]).T)
+    #     self.last_ack_angle = ack_angle
+    #     print("ack_angle", ack_angle)
+    #     return ack_angle
+
+    # detect whether there is a wall in front of the car
+    def wall_in_front(self, msg):
+        angle_min = msg.angle_min
+        angle_max = msg.angle_max
+        angle_increment = msg.angle_increment
+        laserpt_num = len(msg.ranges)
+        angle_list = np.arange(angle_min, angle_max, angle_increment)
+
+        split = int(laserpt_num/10) # pi/3 in the front to dectect wall
+        middle= int(laserpt_num/2)
+        angle_front = angle_list[middle-split:middle+split]
+        dist_all  = np.cos(angle_front)*msg.ranges[middle-split:middle+split]
+        
+        
+        if min(dist_all) < self.wheel_base*self.VELOCITY: # a super simple safety controller
+            print(min(dist_all))
+            return [True,True]
+        if min(dist_all) < self.DESIRED_DISTANCE+self.wheel_base*self.VELOCITY:
+            return [True,False]
         else:
-        	line_ranges, line_angles = self.select_data(ranges,angles,self.left,0)
+            return [False,False]
 
+    # segment the laser data and fit it into a line and visualize
+    # currently only start from the start or the end
+    """
+    def find_line(self, msg, line_pub):
+        angle_min = msg.angle_min
+        angle_max = msg.angle_max
+        angle_increment = msg.angle_increment
+        laserpt_num = len(msg.ranges)
+        angle_list = np.arange(angle_min, angle_max, angle_increment)            
 
-        min_dist = min(line_ranges)
-        rho = [] # distances
-        phi = [] # angles
-        for i in range(len(line_ranges)):
-                if (line_ranges[i] > self.min_thresh) and  (line_ranges[i] - min_dist < self.max_thresh):
-                    rho.append(line_ranges[i])
-                    phi.append(line_angles[i])
-        polar_coordinate = zip(rho,phi)
-        num = len(rho)
-        k, b = self.least_square(polar_coordinate, num)
-        return k, b
-
-    def find_line_pub(self, ranges, angles):
-        # use min distance except when min_ind on wrong side
-        if self.SIDE == -1: # right wall
-            line_ranges, line_angles = self.select_data(ranges,angles,self.right,0)
+        # get how many number of the points we need
+        if self.laser_angle > (angle_max-angle_min):
+            rospy.loginfo("laser scan range out of bound")
+            num = laserpt_num # TODO: may cause issue, fix later
         else:
-            line_ranges, line_angles = self.select_data(ranges,angles,self.left,0)
+            num = np.round(self.laser_angle/(angle_max-angle_min)*laserpt_num)
+            num = int(num)
+        
+        # use min distance except when min_ind on wrong side
+        min_dist = min(msg.ranges)
+        min_ind  = msg.ranges.index(min_dist)
+        # if (self.SIDE == -1 and min_ind<int(laserpt_num/2)) or (self.SIDE == 1 and min_ind>int(laserpt_num/2)):
+        offset = np.round(self.laser_offset/(angle_max-angle_min)*laserpt_num)
+        offset = int(offset)
+        if self.SIDE == -1: # right wall
+            laser_line_fit = msg.ranges[offset:num+offset] # TODO: correspondance check 
+            angle = angle_list[offset:num+offset]
+            min_dist = min(laser_line_fit)
 
+            x_min = laser_line_fit[0]*np.cos(angle[0])
+            x_max = laser_line_fit[-1]*np.cos(angle[-1])
+            # polar_coordinate = zip(angle, laser_line_fit)
+            polar_coordinate = []
+            for i in range(len(laser_line_fit)):
+                if laser_lin_fit[i] > 0.2 and  laser_lin_fit[i] - min_dist < self.thresh:
+                    polar_coordinate.append(angle[i],laser_lin_fit[i])
+            #polar_coordinate = np.vstack((angle, laser_line_fit))
+            #polar_coordinate_f1 = polar_coordinate[1,:] - min_dist < self.thresh
+            #polar_coordinate_f2 = polar_coordinate_f1[1,:]  > 0.2
+            #print(polar_coordinate_f2.shape)
+        elif self.SIDE == 1:
+            laser_line_fit = msg.ranges[-num-offset:-offset]
+            angle = angle_list[-num-offset:-offset]
+            min_dist = min(laser_line_fit)
+            x_max = laser_line_fit[0]*np.cos(angle[0])
+            x_min = laser_line_fit[-1]*np.cos(angle[-1])
+            # polar_coordinate = zip(angle, laser_line_fit)
+            for i in range(len(laser_line_fit)):
+                if laser_lin_fit[i] > 0.2 and  laser_lin_fit[i] - min_dist < self.thresh:
+                    polar_coordinate.append(angle[i],laser_lin_fit[i])
+            #polar_coordinate = np.vstack((angle, laser_line_fit))
+            #polar_coordinate_f1 = polar_coordinate[1,:] - min_dist < self.thresh
+            #polar_coordinate_f2 = polar_coordinate_f1[1,:]  > 0.2
+            #print(polar_coordinate_f2.shape)
+        else:
+            rospy.loginfo("error: input correct side")
+        # strmsg = String()
+        # strmsg.data = "now in normal mode............."
+        # self.debug_pub.publish(strmsg)
+        # else:
+        #     num_half = int(num/2.0)  # min pt in the middle, get the pts around it
+        #     if num_half>min_ind+1:
+        #         num_half = min_ind;
+        #     elif num_half>laserpt_num-1-min_ind:
+        #         num_half = laserpt_num-1-min_ind;
+        #     start_ind= min_ind-num_half
+        #     end_ind  = min_ind+num_half
+        #     x_max = msg.ranges[start_ind]*np.cos(angle_list[start_ind])
+        #     x_min = msg.ranges[end_ind]*np.cos(angle_list[end_ind])
+        #     polar_coordinate = zip(angle_list[start_ind:end_ind], msg.ranges[start_ind:end_ind])
+        #     strmsg = String()
+        #     strmsg.data = "now in min mode"
+        #     self.debug_pub.publish(strmsg)
 
-        min_dist = min(line_ranges)
-        rho = [] # distances
-        phi = [] # angles
-        for i in range(len(line_ranges)):
-                if (line_ranges[i] > self.min_thresh) and  (line_ranges[i] - min_dist < self.max_thresh):
-                    rho.append(line_ranges[i])
-                    phi.append(line_angles[i])
-        x = rho*np.cos(phi)
-        y = rho*np.sin(phi)
-        #print(x,y)  
-        x_min = rho[0]*np.cos(phi[0])
-        x_max = rho[-1]*np.cos(phi[-1])
-        polar_coordinate = zip(rho,phi)
-        num = len(rho)
         k, b = self.least_square(polar_coordinate, num)
-        #k,b = np.polyfit(x,y,1)
+
         x_marker = np.linspace(x_min, x_max, num=20)
         y_marker = k*x_marker + b
-        VisualizationTools.plot_line(x_marker, y_marker, self.line_pub, frame="/laser")
+        VisualizationTools.plot_line(x_marker, y_marker, line_pub, frame="/laser")
+
+        return k, b
+"""
+    def find_line_aggr(self, msg, line_pub):
+        angle_min = msg.angle_min
+        angle_max = msg.angle_max
+        angle_increment = msg.angle_increment
+        laserpt_num = len(msg.ranges)
+        angle_list = np.arange(angle_min, angle_max, angle_increment)            
+
+        # get how many number of the points we need
+        rho = []
+        phi = []
+        # use min distance except when min_ind on wrong side
+        half_ind = int((laserpt_num*0.6))
+        if self.SIDE == -1: # right wall
+            laser_line_fit = msg.ranges[:half_ind] # TODO: correspondance check 
+            angle = angle_list[:half_ind]
+            min_dist = min(laser_line_fit)
+            for i in range(len(laser_line_fit)):
+                if laser_line_fit[i] > 0.2 and  laser_line_fit[i] - min_dist < self.thresh:
+                    rho.append(angle[i])
+                    phi.append(laser_line_fit[i])
+            x_min = rho[0]*np.cos(phi[0])
+            x_max = rho[-1]*np.cos(phi[-1])
+            polar_coordinate = zip(rho,phi)
+        elif self.SIDE == 1:
+            laser_line_fit = msg.ranges[-half_ind:]
+            angle = angle_list[-half_ind:]
+            min_dist = min(laser_line_fit)
+            for i in range(len(laser_line_fit)):
+                if laser_line_fit[i] > 0.2 and  laser_line_fit[i] - min_dist < self.thresh:
+                    rho.append(angle[i])
+                    phi.append(laser_line_fit[i])
+            polar_coordinate = zip(rho, phi)
+            x_min = rho[0]*np.cos(phi[0])
+            x_max = rho[-1]*np.cos(phi[-1])
+        else:
+            rospy.loginfo("error: input correct side")
+
+        # strmsg = String()
+        # strmsg.data = "now in normal mode............."
+        # self.debug_pub.publish(strmsg)
+        # else:
+        #     num_half = int(num/2.0)  # min pt in the middle, get the pts around it
+        #     if num_half>min_ind+1:
+        #         num_half = min_ind;
+        #     elif num_half>laserpt_num-1-min_ind:
+        #         num_half = laserpt_num-1-min_ind;
+        #     start_ind= min_ind-num_half
+        #     end_ind  = min_ind+num_half
+        #     x_max = msg.ranges[start_ind]*np.cos(angle_list[start_ind])
+        #     x_min = msg.ranges[end_ind]*np.cos(angle_list[end_ind])
+        #     polar_coordinate = zip(angle_list[start_ind:end_ind], msg.ranges[start_ind:end_ind])
+        #     strmsg = String()
+        #     strmsg.data = "now in min mode"
+        #     self.debug_pub.publish(strmsg)
+        num = len(rho)
+        k, b = self.least_square(polar_coordinate, num)
+
+        x_marker = np.linspace(x_min, x_max, num=20)
+        y_marker = k*x_marker + b
+        VisualizationTools.plot_line(x_marker, y_marker, line_pub, frame="/laser")
+        dist = np.abs(b)/np.sqrt(1+k**2)
+        #err_msg = Float32()
+        #err_msg
+        self.error_pub.publish(dist-self.DESIRED_DISTANCE)
         return k, b
 
 
@@ -212,7 +302,8 @@ class WallFollower:
         H = np.zeros([count, 2])
         y_col = np.zeros([count, 1])
         counter = 0
-        for dist, ang  in polar_coordinate:
+        # polar_coordinate_new = zip(polar_coordinate[0,:], polar_coordinate[1,:])
+        for ang, dist in polar_coordinate:
             x = np.cos(ang)*dist
             y = np.sin(ang)*dist
             H[counter,0] = x
