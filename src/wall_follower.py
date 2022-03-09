b8014e15dabd71d70bc9b6283ebdc58160ca5248
meenu-s <mrsingh0306@gmail.com>
2022-03-09 13:20:22 -0500

    Adding bag files

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -21,13 +21,13 @@ class WallFollower:
     t_a = 0.34 # max turning angle
     t_r = 0.75 # max turn radius of center of gravity of car 
     w_b = 0.325 # distance between front and rear axles
-    L1 = 1.3 # Look ahead distance for pure pursuit controller 1.3 works
-    max_thresh = 2.6 # remove distant wall noise from lidar 2.6 works
+    L1 = DESIRED_DISTANCE + 0.3 # Look ahead distance for pure pursuit controller 1.3 works
+    max_thresh = 5 # remove distant wall noise from lidar 0.7 works
     min_thresh = 0.1 # remove car noise from lidar
-    front = (0.5,0.1) #offset and split of front of car detection for safety controller
-    right = (0.3,0.55) # start and end fraction for selection of lidar data for right side of car (0.3,0.55) works
+    front = (0.5,0.15) #offset and split of front of car detection for safety controller
+    right = (0.0,0.60) # start and end fraction for selection of lidar data for right side of car (0.3,0.55) works
     left = (1.0-right[1],1.0-right[0]) # start and end fraction for selection of lidar data for left side of car mirrior rights selection
-    s_w = 0.5 # width of safety space 0.5 default wider than that is safer
+    s_w = 0.65 # width of safety space 0.5 default wider than that is safer
 
     #Linearized Pursuit PD controls
     VLratio = VELOCITY/L1
@@ -36,18 +36,21 @@ class WallFollower:
     Dgain = 2
     def __init__(self):
         # Initialize your publishers and subscribers here
-        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=10)
-        self.line_pub = rospy.Publisher(self.WALL_TOPIC, Marker, queue_size=10)
-        self.err_pub = rospy.Publisher('Cur_Error', Float32, queue_size=10)
+        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size=1)
+        self.line_pub = rospy.Publisher(self.WALL_TOPIC, Marker, queue_size=1)
+        self.err_pub = rospy.Publisher('Cur_Error', Float32, queue_size=1)
         self.count = 0
+        self.Moving = True
         self.toterr = 0
-        rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback, (self.pub, self.line_pub))
+        self.turn_msg = AckermannDriveStamped()
+        rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.callback, (self.pub, self.line_pub, self.turn_msg))
 
     # Callback function
     def callback(self, msg, pub_list):
-        turn_msg = AckermannDriveStamped()
+        
         pub = pub_list[0]
         line_pub = pub_list[1]
+        turn_msg = pub_list[2]
 
         # rad and angle values for polar coordinates of laser data
         angle_min = msg.angle_min
@@ -67,20 +70,15 @@ class WallFollower:
 
         #turn_msg publishing
         turn_msg.header.stamp = rospy.Time.now()
-        turn_msg.drive.speed = 0 if stop else self.VELOCITY
+        if stop:
+            turn_msg.drive.speed = -0.5*(self.Moving)
+            self.Moving = False
+        else:
+            self.Moving = True
+            turn_msg.drive.speed = self.VELOCITY
+
         turn_msg.drive.steering_angle = ack_angle
         pub.publish(turn_msg)      
-
-    # error recording
-    def rec_error(self, error):
-        cur_error = abs(error)
-        a = 0.6
-        self.count += 1.0
-        score = 1/(1+(a*cur_error)**2)
-        self.toterr += score
-        error = self.toterr/self.count
-        self.err_pub.publish(error)
-        return error
 
     # Select laser data with left fraction and right fraction or
     # Set selection type to 1 to select with offset and split range
@@ -121,31 +119,36 @@ class WallFollower:
 
     # Safety controller
     def safety(self, ranges, angles):
-        tr = self.t_r 
-        sw = self.s_w/2.0
+        angle = self.turn_msg.drive.steering_angle
+        neg = 1 if angle > 0 else -1
+        next_ang = neg*max((abs(angle) + 0.05),0.34)
+        tr = self.w_b/np.tan(next_ang)
+        sw = self.s_w
         s = -self.SIDE
         s_ranges, s_angles = self.select_data(ranges,angles,self.front,1)
-
         polar_coordinate = zip(s_ranges,s_angles)
         for rho,phi in polar_coordinate:
-            if rho < tr:
+            if rho < 100:
                 x = rho*np.cos(phi)
                 y = rho*np.sin(phi)
                 out_circle = ((x+(s*tr))**2) + y**2 < ((tr+sw)**2)
                 inn_circle = ((x+(s*tr))**2) + y**2 > ((tr-sw)**2)
-                if out_circle and inn_circle:
+                infront = y > 0
+                if out_circle and inn_circle and infront:
+                    #print("stop")
                     return True
         return False
 
     # Pure pursuit controller
     def PPController(self, k, b):
         d = abs(b/(math.sqrt(1+(k**2)))) - self.DESIRED_DISTANCE
+        self.err_pub.publish(d)
         ang = max(min(1.0,d/self.L1),-1.0)
         n1 = np.arcsin(ang)
         n2 = self.SIDE*np.arctan(k)
         n = n1 + n2
         #self.rec_error(d)
-        print(self.rec_error(d))
+        #print(self.rec_error(d))
         ackman_ang = np.arctan((2*self.w_b*np.sin(n))/self.L1)
         return self.SIDE*ackman_ang
 
